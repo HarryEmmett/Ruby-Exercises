579b4357f1f765e957f5a29b3a5d5a97387ac04f
harry <harry_emmett@outlook.com>
2023-09-06 20:13:58 +0100

    fix: remove variable assignment

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,48 +1,45 @@
 def caesar_cipher(string, factor)
-    result = ''
-    val = ''
-    mapped = string.each_char do |char|
-      # (char + factor) % 26 )
-      u_case = char.ord >= 65 && char.ord <= 90
-      l_case = char.ord >= 97 && char.ord <= 122
-      val = l_case ? char.ord - 65 + factor % 26 + 65 : char.ord - 97 + factor % 26 + 97
-      if u_case
-        result += val > 90 ? (val - 26).chr : val.chr
-      elsif l_case
-        result += val > 122 ? (val - 26).chr : val.chr
-      else
-        result += char
-      end
-    end
-    result
+  result = ''
+  string.each_char do |char|
+    # (char + factor) % 26 )
+    u_case = char.ord >= 65 && char.ord <= 90
+    l_case = char.ord >= 97 && char.ord <= 122
+    startOrd = l_case ? 97 : 65
+    result += if u_case || l_case
+                ((((char.ord - startOrd) + factor) % 26) + startOrd).chr
+              else
+                char
+              end
   end
-  
-  # def caesar_cipher(str, factor)
-  #   lowcase_str = str
-  #   res = lowcase_str.split('').map do |char|
-  #     if char.ord < 97 || char.ord > 122
-  #       char
-  #     else
-  #       cipher = char.ord + factor
-  #       offset = factor.abs % 26
-  
-  #       if cipher < 97
-  #         if cipher - factor - offset < 97
-  #           (cipher - factor - offset + 26).chr
-  #         else
-  #           (cipher - factor - offset).chr
-  #         end
-  
-  #       elsif cipher > 122
-  #         if  cipher - factor + offset > 122
-  #           (cipher - factor + offset - 26).chr
-  #         else
-  #           (cipher - factor + offset).chr
-  #         end
-  #       else
-  #         (char.ord + factor).chr
-  #       end
-  #     end
-  #   end
-  #   res.join('')
-  # end
+  result
+end
+
+# def caesar_cipher(str, factor)
+#   lowcase_str = str
+#   res = lowcase_str.split('').map do |char|
+#     if char.ord < 97 || char.ord > 122
+#       char
+#     else
+#       cipher = char.ord + factor
+#       offset = factor.abs % 26
+
+#       if cipher < 97
+#         if cipher - factor - offset < 97
+#           (cipher - factor - offset + 26).chr
+#         else
+#           (cipher - factor - offset).chr
+#         end
+
+#       elsif cipher > 122
+#         if  cipher - factor + offset > 122
+#           (cipher - factor + offset - 26).chr
+#         else
+#           (cipher - factor + offset).chr
+#         end
+#       else
+#         (char.ord + factor).chr
+#       end
+#     end
+#   end
+#   res.join('')
+# end
