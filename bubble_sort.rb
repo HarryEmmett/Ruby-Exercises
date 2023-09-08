ce9329a72a0ddb870c7220e08077f15473959eb6
harry <harry_emmett@outlook.com>
2023-09-08 22:20:46 +0100

    remove extra loop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,7 @@
 def bubble_sort(arr)
   index = 0
-  while index != arr.length
+  # n - 1 loops index = 0 so need -2
+  while index != arr.length - 2
     for a in 0..arr.length - 1 do
       if a == arr.length - 1
         break
@@ -15,11 +16,12 @@ def bubble_sort(arr)
   arr
 end
 
+p bubble_sort([3, 2, 1, 5])
 p bubble_sort([6, 8, 5, 2, 3, 4])
 p bubble_sort([4, 3, 78, 2, 0, 2])
 p bubble_sort([4, 3, 78, 23, 0, 211])
 p bubble_sort([47, 34, 758, 26, 3, 2])
-p bubble_sort([9,8,7,6,5,4,3,2,1,0])
+p bubble_sort([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
 
 # def bubble_sort(arr)
 #     arr.sort do |a, b|
